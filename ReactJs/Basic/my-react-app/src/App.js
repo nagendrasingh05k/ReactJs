26d708057555135696984fd1b55a44832fd43a27
nagendrasingh05k <nagendra9395a@gmail.com>
2025-03-03 17:29:39 +0530

    updated code

--- ReactJs/Basic/my-react-app/src/App.js
+++ ReactJs/Basic/my-react-app/src/App.js
@@ -4,21 +4,16 @@ import './App.css';
 import React, { useState } from 'react';
  
 function App() {
-  // useState hook to manage state in the component
   const [count, setCount] = useState(0);
  
-  // Function to increment the count
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
  
   return (
-<div className="App">
-<header className="App-header">
-<h1>Welcome to My React App</h1>
-<p>You clicked {count} times</p>
-<button onClick={incrementCount}>Click me</button>
-</header>
+<div>
+<h1>Counter: {count}</h1>
+<button onClick={increment}>Increment</button>
+<button onClick={decrement}>Decrement</button>
 </div>
   );
 }
