ec9894a1cfab3e58f0e8dd64cb65e48ee99f4c2b
nagendrasingh05k <nagendra9395a@gmail.com>
2025-03-04 12:06:22 +0530

    new file added 4mar

---
+++ ReactJs/Basic/router/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      {/* Use <Routes> to wrap <Route> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
