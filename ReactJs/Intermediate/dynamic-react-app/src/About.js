368049f6b2a8074166ca66f84c546da1939dcd53
nagendrasingh05k <nagendra9395a@gmail.com>
2025-03-08 16:57:30 +0530

    add new file min-capstone 8 mar

---
+++ ReactJs/Intermediate/dynamic-react-app/src/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className='container'>
+      <h1>About Us</h1>
+      <p>Welcome to our app! We are dedicated to creating efficient and user-friendly solutions for various needs.</p>
+      <h2>Who We Are</h2>
+      <p>Our platform is built by a passionate developer with expertise in Java, React, Excel,
+         and Document Analytics. With a background in data breach consulting, we understand the importance
+          of security, accuracy, and efficiency in digital solutions.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ ReactJs/Intermediate/dynamic-react-app/src/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div className='container'>
+      <h1>Contact Us</h1>
+      <p>YWe’d love to hear from you! Whether you have questions, feedback, or collaboration
+         ideas, feel free to reach out.</p>
+
+         <p><strong>Email:</strong>nagendra@gmail.com</p>
+         We’ll get back to you as soon as possible!
+    </div>
+  );
+};
+
+export default Contact;
